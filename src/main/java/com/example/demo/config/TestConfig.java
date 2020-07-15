799f46a206cab6db1df88e8ae3f5daceaddce901
liujq-h <liujq-h@glodon.com>
2020-07-15 08:34:25 +0800

    测试修改

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.demo.domain.TestBean;
 
+/**
+ * test
+ * @author liujq-h
+ *
+ */
 //@Configuration
 public class TestConfig {
 	
